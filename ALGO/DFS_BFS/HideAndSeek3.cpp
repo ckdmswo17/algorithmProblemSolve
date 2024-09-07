17aa38d98bb8efcb170f143c4d52dfc9ae6925c6
ckdmswo17 <ckdmswo17@naver.com>
2024-09-08 00:59:49 +0900

    add 1 dfs problem HideAndSeek3

--- ALGO/DFS_BFS/HideAndSeek3.cpp
+++ ALGO/DFS_BFS/HideAndSeek3.cpp
@@ -1,36 +1,44 @@
-// 백준 문제 : 숨바꼭질3 (미완)
+// 백준 문제 : 숨바꼭질3
 
 #include <iostream>
 #include <queue>
 
 using namespace std;
 
-int dx[3] = {0,1,-1};
+int dx[3] = {0,-1,1};
 int N,K;
 vector<int> dist(200002,0);
 vector<bool> visited(200002,false);
 queue<int> qu;
 
 int bfs(){
+    if(N >= K){
+        return N-K;
+    }
     qu.push(N);
     visited[N] = true;
     while(!qu.empty()){
         int x = qu.front(); qu.pop();
+        // cout << "큐 반복 : " << x << endl;
 
-        if(x == K){
-            return dist[x];
+        if(visited[K] == true){
+            return dist[K];
         }
         for(int i=0;i<3;i++){
             int cx;
             if(i == 0){
                 cx = x*2;
-                while(cx < 200002){
+                while(cx < K + (K-N)){
                     if(visited[cx] == false){
+                        // cout << cx << endl;
                         dist[cx] = dist[x];
                         visited[cx] = true;
                         qu.push(cx);
+                        cx *= 2;
+                    } else {
+                        break;
                     }
-                    cx = cx*2;
+                    
                 }
                 continue;
             } else {
@@ -38,10 +46,11 @@ int bfs(){
             }
 
             
-            if(cx >= 0 && cx < 200002 && visited[cx] == false){
+            if(cx >= 0 && cx < K+(K-N) && visited[cx] == false){
                 dist[cx] = dist[x]+1; 
                 visited[cx] = true;
-                qu.push(cx); 
+                qu.push(cx);
+                // cout << cx << endl;
             }
         }
     }
