ec676b264d391340074fd04a1416f9a83414b494
ckdmswo17 <ckdmswo17@naver.com>
2024-10-24 23:33:01 +0900

    add 1 Math problem MakePrime

---
+++ ALGO/Math/MakePrime.cpp
@@ -0,0 +1,28 @@
+// 백준 문제 : 소수 구하기
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int M,N;
+    cin >> M >> N;
+
+    bool isPrime[N+1];
+    fill_n(isPrime,N+1,true);
+    isPrime[1] = false;
+
+    for(int i=2;i<N+1;i++){
+        if(isPrime[i] == true){
+            for(int j=2;i*j<N+1;j++){
+                isPrime[i*j] = false;
+            }
+        }
+    }
+
+    for(int i=M;i<N+1;i++){
+        if(isPrime[i] == true){
+            cout << i << "\n";
+        }
+    }
+}
