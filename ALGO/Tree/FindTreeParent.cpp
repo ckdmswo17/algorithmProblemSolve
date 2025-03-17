f1b5a6b1122a6bd1c8e35d7a4c9e260fe0fc94d3
ckdmswo17 <ckdmswo17@naver.com>
2025-03-17 21:52:12 +0900

    add 1 Tree Problem FindTreeParent

--- ALGO/Tree/FindTreeParent.cpp
+++ ALGO/Tree/FindTreeParent.cpp
@@ -1,64 +1,42 @@
-// 백준 문제 : 트리의 부모 찾기 (미완)
+// 백준 문제 : 트리의 부모 찾기
 
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-int N;
-vector<vector<int>> trees;
-int parents[100001];
-
-void parentsFind(int i, int beforeE)
-{
-    bool isEnd = false;
-    int j = 0;
-    while(isEnd == false){
-        // cout << isEnd <<  " "<< trees[i][j] << "\n";
-        if(trees[i][j] == 0){
-            isEnd = true;
-            break;
-        }
-        if(beforeE == trees[i][j]){
-            parents[i] = trees[i][j];
-        } else {
-            parentsFind(trees[i][j],i);
-        }
-        j += 1;
-    }
-    return;
-}
-
 int main(){
-    ios_base :: sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
+    int N;
     cin >> N;
 
-    // trees(N+1,vector<int>(N,0));
-    for(int i=0;i<N-1;i++){
+    vector<int> adj[N+1];
+    for(int i=1;i<N;i++){
         int e1,e2;
         cin >> e1 >> e2;
+        adj[e1].push_back(e2);
+        adj[e2].push_back(e1); 
+    }
 
-        int j = 0;
-        while(trees[e1][j] != 0){
-            j += 1;
-        }
-        trees[e1][j] = e2;
-
-        int z = 0;
-        while(trees[e2][z] != 0){
-            z += 1;
-        }
-        trees[e2][z] = e1;
+    int p[N+1];
+    queue<int> qu;
+    qu.push(1);
+    while(!qu.empty()){
+        int cur = qu.front();
+        qu.pop();
         
+        for(auto nxt:adj[cur]){
+            if(p[cur] == nxt) continue;
+            qu.push(nxt);
+            p[nxt] = cur;
+        }
     }
 
-    parentsFind(1,0);
     for(int i=2;i<N+1;i++){
-        cout << parents[i] << "\n";
+        cout << p[i] << "\n";
     }
 
-    
 }
