305e92e0c4df25d4e649dbba1bc605bb25192c62
ckdmswo17 <ckdmswo17@naver.com>
2025-06-27 23:17:11 +0900

    add 1 BasicCodingProblem (25.06.27)

---
+++ BasicCoding/FastA+B.cpp
@@ -0,0 +1,19 @@
+// 백준 문제 : 카드 역배치
+
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    for(int i=0;i<T;i++){
+        int A,B;
+        cin >> A >> B;
+        cout << A+B << "\n";
+    }
+}
