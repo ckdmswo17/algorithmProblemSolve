a377d482d4b2da414e8949b4f278a5f56a818329
ckdmswo17 <ckdmswo17@naver.com>
2025-07-08 22:22:08 +0900

    add 1 BasicCoding problem (25.07.08)

---
+++ BasicCoding/StarPrint7.cpp
@@ -0,0 +1,35 @@
+// 백준 문제 : 별 찍기 - 6
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    // 상단부, 중간
+    for(int i=1;i<=N;i++){
+        for(int j=0;j<N-i;j++){
+            cout << " ";
+        }
+        for(int j=0;j<2*i-1;j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    // 하단부
+    for(int i=1;i<N;i++){
+        for(int j=0;j<i;j++){
+            cout << " ";
+        }
+        for(int j=0;j<2*N-(1+2*i);j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
