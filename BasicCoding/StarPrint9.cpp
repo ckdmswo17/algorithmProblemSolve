98363a280af2da9522d93f5881164a6883f8ebb5
ckdmswo17 <ckdmswo17@naver.com>
2025-08-10 19:06:18 +0900

    별 찍기 - 9/브론즈3/메모리 2020KB/시간 0ms

---
+++ BasicCoding/StarPrint9.cpp
@@ -0,0 +1,37 @@
+// 백준 문제 : 별 찍기 - 9
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    bool beforeHalf = true;
+    // 줄 반복
+    for(int i=1;i<=2*N-1;i++){
+        if(beforeHalf == true){
+            for(int j=1;j<i;j++){
+                cout << " ";
+            }
+            for(int j=0;j<2*N-2*(i-1)-1;j++){
+                cout << "*";
+            }
+            if(i == (int)((2*N-1)/2+1)){
+                beforeHalf = false;
+            }
+        } else {
+            for(int j=1;j<N-(i%N);j++){
+                cout << " ";
+            }
+            for(int j=0;j<1+2*(i%N);j++){
+                cout << "*";
+            }
+        }
+        cout << "\n";
+    }
+}
