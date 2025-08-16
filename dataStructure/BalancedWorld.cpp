764aa25efee33e41da491956b4346ca4ec575509
ckdmswo17 <ckdmswo17@naver.com>
2025-08-16 21:32:08 +0900

    균형잡힌 세상/실버4/메모리 2024KB/시간 4ms

---
+++ dataStructure/BalancedWorld.cpp
@@ -0,0 +1,46 @@
+// 백준 문제 : 균형잡힌 세상
+
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+    string str;
+    getline(cin,str);
+	while(str[0] != '.'){
+        stack<char> sc;
+
+        // 문자열 처리
+        for(const char c:str){
+            if(c == '('){
+                sc.push('(');
+            } else if(c == '['){
+                sc.push('[');
+            } else if(c == ')'){
+                if(!sc.empty() && sc.top() == '(') {
+                    sc.pop();
+                } else {
+                    sc.push(')');
+                }
+            } else if(c == ']'){
+                if(!sc.empty() && sc.top() == '[') {
+                    sc.pop();
+                } else {
+                    sc.push(']');
+                }
+            }
+        }
+
+        // yes or no 출력
+        if(sc.empty()) cout << "yes\n";
+        else cout << "no\n"; 
+
+        // 다음 반복을 위한 문자열 입력받기
+        getline(cin,str);
+    }
+}
