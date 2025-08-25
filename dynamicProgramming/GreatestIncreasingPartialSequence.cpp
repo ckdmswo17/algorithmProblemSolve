4c63f5f8394a4948b33e2d5a2c93d00f97dcc2af
ckdmswo17 <ckdmswo17@naver.com>
2025-08-25 22:06:47 +0900

    가장 큰 증가하는 부분 수열/실버2/메모리 2020KB/시간 0ms

---
+++ dynamicProgramming/GreatestIncreasingPartialSequence.cpp
@@ -0,0 +1,43 @@
+// 백준 문제 : 가장 큰 증가하는 부분 수열
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    int A[N]; // 원본 배열
+    int S[N]; // 인덱스 i까지의 최대 증가 부분 수열 합
+
+    for(int i=0;i<N;i++){
+        cin >> A[i];
+        S[i] = A[i];
+    }
+
+    int ans = S[0];
+    for(int i=0;i<N;i++){
+        for(int j=0;j<i;j++){
+            // i로 각 원소 반복
+            // j로 i보다 작을 동안 이전 원소 반복
+            if(A[j] < A[i]) { // 두 요소가 증가하는 관계에 있을 때
+                // i까지의 원래 합과
+                // j까지의 원래 합 + 새로운 요소
+                // 대소 비교해서 합 배열에 저장 
+                S[i] = max(S[i],S[j]+A[i]); 
+
+                if(ans < S[i]) {
+                    ans = S[i];
+                }
+            }
+        }
+    }
+
+    cout << ans << "\n";
+
+}
