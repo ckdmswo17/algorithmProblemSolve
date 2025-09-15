510dc1ce4dbbb456876ffdaf45ecc88b1502d776
ckdmswo17 <ckdmswo17@naver.com>
2025-09-15 17:10:44 +0900

    A-B/브론즈5/메모리 2020KB/시간 0ms

---
+++ Math/BOJ1001.cpp
@@ -0,0 +1,15 @@
+// 백준 문제 : A-B
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int A,B;
+    cin >> A >> B;
+    
+    cout << A-B << "\n";
+}
