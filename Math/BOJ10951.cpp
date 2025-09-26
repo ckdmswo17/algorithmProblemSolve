a218ed75d3216b8199ac36b0c7583ebdbef73876
ckdmswo17 <ckdmswo17@naver.com>
2025-09-26 15:31:14 +0900

    A+B - 4/브론즈5/메모리 2020KB/시간 0ms

---
+++ Math/BOJ10951.cpp
@@ -0,0 +1,15 @@
+// 백준 문제 : A+B - 4
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int A,B;
+    while(cin >> A >> B){
+        cout << A+B << "\n";
+    }
+}
