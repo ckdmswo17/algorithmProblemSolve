6b071441096090a620258968d6f092b1c2c7be75
ckdmswo17 <ckdmswo17@naver.com>
2025-09-26 15:32:49 +0900

    아스키 코드/브론즈5/메모리 2020KB/시간 0ms

---
+++ greedy_implement/BOJ11654.cpp
@@ -0,0 +1,14 @@
+// 백준 문제 : 아스키 코드
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    char c;
+    cin >> c;
+    cout << (int)c << "\n";
+}
