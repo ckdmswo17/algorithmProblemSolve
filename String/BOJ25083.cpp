eeff0308f6ac4b13d46786727b281678b8fa165c
ckdmswo17 <ckdmswo17@naver.com>
2025-10-02 02:01:15 +0900

    새싹/브론즈5/메모리 2020KB/시간 0ms

---
+++ String/BOJ25083.cpp
@@ -0,0 +1,11 @@
+// 백준 문제 : 새싹
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cout << "         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |";
+}
